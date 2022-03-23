78a8569986cf8d43a6e59b78f4a053699493119a
xoang <xoanfuerteventura@gmail.com>
2022-03-23 01:48:50 +0100

    primer commit

---
+++ src/main/java/com/mycompany/git/Sumador.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.git;
+
+/**
+ *
+ * @author xoang
+ */
+public class Sumador {
+
+    public static float sumar(float a, float b) {
+        return a + b;
+    }
+
+}
